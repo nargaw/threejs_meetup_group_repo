4c4731fefc94233b39f15fb59ac06a3197d84d72
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2024-05-28 08:28:57 -0400

    update mouse tracker

--- meetup_11/src/Experience.jsx
+++ meetup_11/src/Experience.jsx
@@ -4,12 +4,29 @@ import CursorShader from './CursorShader'
 
 export default function Experience()
 {
+
+    const madeWithLoveStyle = {
+        fontFamily: 'MadimiOne, sans-serif',
+        position: 'absolute',
+        left: '100%',
+        top: '80%',
+        width: '100%',
+        transform: 'translate(-50%, -50%)',
+        zIndex: '2',
+        color: '#f4a261',
+    }
+
+    const spanStyle = {
+
+    }
+
     return(
         <> 
             <Canvas >
                 {/* <OrbitControls />  */}
                 <CursorShader />
             </Canvas>
+            <p style={madeWithLoveStyle}>made with <span>&#9825;</span> by Sost Studio</p>
         </>
     )
 }
