c1042e754d414d06c6201d6ee8b05655c6d36b68
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2024-06-29 05:07:59 -0400

    refactor

--- msdf_text/src/Shaders/vertex.glsl
+++ msdf_text/src/Shaders/vertex.glsl
@@ -35,35 +35,6 @@ varying float vLetterIndex;
 uniform vec2 u_mouse;
 uniform float u_time;
 
-//easing functions
-float easeOutBounce(float x){
-    const float nl = 7.5625;
-    const float dl = 2.75;
-
-    if (x < 1. / dl){
-        return nl * x * x;
-    } else if (x < 2.0 / dl){
-        x -= 1.5/ dl;
-        return nl * x * x + 0.75;
-    } else if (x < 2.5 / dl){
-        x -= 2.25 / dl;
-        return nl * x * x + 0.9375;
-    } else {
-        x -= 2.625 / dl;
-        return nl * x * x + 0.984375;
-    }
-}
-
-float easeInBounce(float x){
-    return 1. - easeOutBounce(1. - x);
-}
-
-float easeInOutBounce(float x){
-    return x < 0.5
-    ? (1. - easeOutBounce(1. - 2. * x)) / 2.
-    : (1. + easeOutBounce(2. * x - 1.)) / 2.;
-}
-
 void main() {
     
     vec3 localSpacePosition = position;
